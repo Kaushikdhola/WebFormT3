21a2d91c12f16f1479ce3386aa4d0ffff36c1710
Kaushik Dhola <kaushikdhola02@gmail.com>
2024-02-14 19:22:36 -0400

    Update profile-form.component.ts

--- src/app/profile-form/profile-form.component.ts
+++ src/app/profile-form/profile-form.component.ts
@@ -95,7 +95,7 @@ export class ProfileFormComponent{
             if (errorMessage) {
               this.toastr.error(errorMessage, 'Validation Error', {
                 positionClass: 'toast-top-right',
-                timeOut: 1500,
+                timeOut: 2000,
               });
               errorDisplayed = true; 
             }
